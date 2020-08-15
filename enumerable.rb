cd544c52ad5c59ca292fb57453860fc916b5363d
Cecilia Benitez <cecibenitezca@gmail.com>
2020-08-15 09:13:27 -0400

    fix use of self in enumerable.rb file

--- enumerable.rb
+++ enumerable.rb
@@ -1,26 +1,30 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/RedundantSelf
+
 # Enumerable module
 module Enumerable
   def my_each
-    if is_a?(Array)
-      length.times { |i| yield(self[i]) }
-    elsif is_a? Hash
-      length.times { |i| yield(keys[i], values[i]) }
+    if self.is_a?(Array)
+      self.length.times { |i| yield(self[i]) }
+    elsif self.is_a?(Hash)
+      self.length.times { |i| yield(self.keys[i], self.values[i]) }
     end
+
+    self
   end
 
   def my_each_with_index
-    length.times do |i|
-      yield(self[i], i)
-    end
+    self.length.times { |i| yield(self[i], i) }
   end
 
   def my_select
-    if is_a?(Array)
+    if self.is_a?(Array)
       results = []
-      length.times { |i| results << self[i] if yield(self[i]) }
-    elsif is_a?(Hash)
+      self.length.times { |i| results << self[i] if yield(self[i]) }
+    elsif self.is_a?(Hash)
       results = {}
-      my_each { |k, v| results[k] = v if yield(k, v) }
+      self.my_each { |k, v| results[k] = v if yield(k, v) }
     end
 
     results
@@ -28,10 +32,10 @@ module Enumerable
 
   def my_all?
     result = true
-    if is_a?(Array)
-      my_each { |item| result = false unless yield(item) }
-    elsif is_a(Hash)
-      my_each { |k, v| result = false unless yield(k, v) }
+    if self.is_a?(Array)
+      self.my_each { |item| result = false unless yield(item) }
+    elsif self.is_a?(Hash)
+      self.my_each { |k, v| result = false unless yield(k, v) }
     end
 
     result
@@ -39,20 +43,20 @@ module Enumerable
 
   def my_any?
     result = false
-    if is_a?(Array)
-      my_each { |item| result = true if yield(item) }
-    elsif is_a?(Hash)
-      my_each { |k, v| result = true if yield(k, v) }
+    if self.is_a?(Array)
+      self.my_each { |item| result = true if yield(item) }
+    elsif self.is_a?(Hash)
+      self.my_each { |k, v| result = true if yield(k, v) }
     end
 
     result
   end
 
   def my_none?
-    if is_a?(Array)
-      result = !my_any? { |item| yield(item) }
-    elsif is_a?(Hash)
-      result = !my_any? { |k, v| yield(k, v) }
+    if self.is_a?(Array)
+      result = !self.my_any? { |item| yield(item) }
+    elsif self.is_a?(Hash)
+      result = !self.my_any? { |k, v| yield(k, v) }
     end
 
     result
@@ -60,16 +64,16 @@ module Enumerable
 
   def my_count(obj = nil)
     result = 0
-    my_each { |item| result += 1 if item == obj || obj.nil? }
+    self.my_each { |item| result += 1 if item == obj || obj.nil? }
     result
   end
 
   def my_map(&block)
     result = []
-    if is_a?(Array)
-      my_each { |item| result << (block.nil? ? yield(item) : block.call(item)) }
-    elsif is_a?(Hash)
-      my_each { |k, v| result << (block.nil? ? yield(k, v) : block.call(k, v)) }
+    if self.is_a?(Array)
+      self.my_each { |item| result << (block.nil? ? yield(item) : block.call(item)) }
+    elsif self.is_a?(Hash)
+      self.my_each { |k, v| result << (block.nil? ? yield(k, v) : block.call(k, v)) }
     end
     result
   end
@@ -77,44 +81,13 @@ module Enumerable
   def my_inject(initial_value = nil)
     initial_value = self[0] if initial_value.nil?
     result = initial_value
-    my_each { |item| result = yield(result, item) }
+    self.my_each { |item| result = yield(result, item) }
     result
   end
 
-  def multiply_els(num)
-    num.my_inject(1) { |t, i| t * i }
+  def multiply_els(numbers)
+    numbers.my_inject(1) { |total, item| total * item }
   end
 end
 
-# list = [5, 4, 3, 2, 1]
-#
-# list.each { |l| puts "#{l}"}
-# puts
-#
-# list.my_each { |l| puts "#{l}"}
-# puts
-#
-# list.my_each_with_index { |l| puts "#{l}"}
-# puts
-#
-# list.my_select { |l| puts "#{l.even?}"}
-# puts
-#
-# list.my_all? { |l| puts "#{l > 3}" }
-# puts
-#
-# list.my_any? { |l| puts "#{l > 3}" }
-# puts
-#
-# list.my_none? { |l| puts "#{l > 3}" }
-# puts
-#
-# list.my_count { |l| puts "#{l}" }
-# puts
-#
-# list.my_map { |l| puts "#{l*l}" }
-# puts
-#
-# list.my_inject { |l| puts "#{l}" }
-# puts
-#
+# rubocop:enable Style/RedundantSelf
