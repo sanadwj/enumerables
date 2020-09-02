d6600575d6df09c6fe86b56f669bd2107da7fecf
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-09-02 11:24:53 -0500

    added my_all, my_any and my_none tests

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,6 +3,8 @@ require_relative '../enumerable'
 describe Enumerable do
   let (:arr) {[1, 2, 3, 4]}
   let (:ans) {[]}
+  let (:bool) {[true, false, nil]}
+  let (:str) {%w(book pen table)}
 
   context "my_each" do
     it "It should check through array and block" do
@@ -21,6 +23,42 @@ describe Enumerable do
   context "my_select" do
     it "Select the items that met the condition " do
       expect(arr.my_select { |x| x % 2 == 0}).to eq([2, 4])
+    end
+  end
+
+  context "my_all?" do
+    it 'Check if a block or the argument are not given and if there is a false or nil' do
+      expect(bool.my_all?).to eq(false)
+    end
+    it 'Check if a block is given' do
+      expect(arr.my_all?{ |x| x.is_a?(Integer) }).to be true
+    end
+    it 'Check if the regex match' do
+      expect(str.my_all? (/oo/)).to eq(false)
+    end
+  end
+
+  context "my_any?" do
+    it 'Check if a block or the argument are not given and if there is a false or nil' do
+      expect(bool.my_any?).to eq(true)
+    end
+    it 'Check if a block is given' do
+      expect(arr.my_any?{ |x| x.is_a?(Integer) }).to be true
+    end
+    it 'Check if the regex match' do
+      expect(str.my_any? (/oo/)).to eq(true)
+    end
+  end
+
+  context "my_none?" do
+    it 'Check if a block or the argument are not given and if there is a false or nil' do
+      expect(bool.my_none?).to eq(false)
+    end
+    it 'Check if a block is given' do
+      expect(arr.my_none?{ |x| x.is_a?(Integer) }).to be false
+    end
+    it 'Check if the regex match' do
+      expect(str.my_none? (/z/)).to eq(true)
     end
   end
 
